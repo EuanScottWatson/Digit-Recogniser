6737543f68005df92d21fb5a60679f58cff0e405
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2021-12-14 23:58:02 +0000

    Added hyperparameter tuning

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+from itertools import permutations
+import sys
 
 
 mnist = tf.keras.datasets.mnist
@@ -10,35 +12,64 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 
-def make_model():
+def make_model(layers=[280, 128, 60]):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten()) # Flatten the 28x28 to 784x1
-    model.add(tf.keras.layers.Dense(280, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(60, activation=tf.nn.relu))
+    for layer in layers:
+        model.add(tf.keras.layers.Dense(layer, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax)) # Only 10 output nodes for 0-9 with a probability distribution of softmax
     return model
 
 
-def train(model):
+def train(model, epochs):
     model.compile(optimizer='adam',
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=3)
+    model.fit(x_train, y_train, epochs=epochs, verbose=0)
 
-    val_loss, val_acc = model.evaluate(x_test, y_test)
-    print(val_loss, val_acc)
+    val_loss, val_acc = model.evaluate(x_test, y_test, verbose=0)
+    # print(val_loss, val_acc)
 
-    model.save("mnist.model") # saving model
+    return val_loss, val_acc
 
 
-def predict(model):
+def tune_hyperparameters():
+    # Generates 180 different network types
+    # Best found was (layers=[250, 150], epochs=5)
+    number_of_epochs = [3, 5, 10]
+    neurons = [50, 100, 150, 250]
+    number_of_layers = [2, 3, 4]
+
+    configs = []
+    for e in number_of_epochs:
+        for l in number_of_layers:
+            for layers in permutations(neurons, l):
+                configs.append((layers, e))
+    
+    best_loss = float('inf')
+    for i, (layer, e) in enumerate(configs):
+        print("%.2f percent complete, Best Loss: %.2f" % (round(i / len(configs) * 100, 3), best_loss), end='\r')
+        sys.stdout.flush()
+
+        model = make_model(layer)
+        loss, acc = train(model, e)
+        if loss < best_loss:
+            best_loss = loss
+            model.save("mnist.model") # saving model
+
+            print("Best config: layers={0}, epochs={1}".format(layer, e))
+            print("\tLoss: {0}".format(loss))
+            print("\tAccuracy: {0}".format(acc))
+    
+
+
+def load_and_predict():
+    model = tf.keras.models.load_model("mnist.model")
     pred = model.predict(np.expand_dims(x_test[123], axis=0))
     print(np.argmax(pred))
 
 
 if __name__ == "__main__":
-    model = make_model()
-    train(model)
-    predict(model)
+    # tune_hyperparameters()
+    load_and_predict()
